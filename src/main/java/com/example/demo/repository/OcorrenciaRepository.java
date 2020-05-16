f9d40720e6ba68762cd01187df09f3c97e047bd9
shalomfernando <shalonando@gmail.com>
2020-05-15 21:10:14 -0300

    heroku

--- src/main/java/com/example/demo/repository/OcorrenciaRepository.java
+++ src/main/java/com/example/demo/repository/OcorrenciaRepository.java
@@ -12,12 +12,12 @@ import com.example.demo.model.Ocorrencia;
 @Repository
 public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Integer>{
 	
-	@Query(value =  "select * from Ocorrencia o where o.tipo_ocorrencia = :tipo and o.usuario_id = :id", nativeQuery = true)
-	public Ocorrencia findByTipo(@Param("tipo") String tipo, @Param("id" ) int id);
-	
-	@Query(value =  "select * from Ocorrencia o where o.usuario_id = :id", nativeQuery = true)
-	public List<Ocorrencia> findAllTodos(@Param("id") int id); 
-	
-	@Query(value =  "SELECT id FROM ocorrencia ORDER BY id DESC LIMIT 1", nativeQuery = true)
-	public Integer findLast(); 
+//	@Query(value =  "select * from Ocorrencia o where o.tipo_ocorrencia = :tipo and o.usuario_id = :id", nativeQuery = true)
+//	public Ocorrencia findByTipo(@Param("tipo") String tipo, @Param("id" ) int id);
+//	
+//	@Query(value =  "select * from Ocorrencia o where o.usuario_id = :id", nativeQuery = true)
+//	public List<Ocorrencia> findAllTodos(@Param("id") int id); 
+//	
+//	@Query(value =  "SELECT id FROM ocorrencia ORDER BY id DESC LIMIT 1", nativeQuery = true)
+//	public Integer findLast(); 
 }
--- src/main/java/com/example/demo/service/OcorrenciaService.java
+++ src/main/java/com/example/demo/service/OcorrenciaService.java
@@ -24,73 +24,79 @@ public class OcorrenciaService implements IOcorrenciaService{
 	
 	public Ocorrencia cadastrar(Ocorrencia ocorrencia) {
 		ocorrencia.setPathFoto(decode(ocorrencia.getPathFoto()));
-		return ocorrenciaRepository.save(ocorrencia);
+//		return ocorrenciaRepository.save(ocorrencia);
+		return null;
 	}
 	
 	public List<Ocorrencia> findAll(int id) {
-		List<Ocorrencia> ocorrencias = ocorrenciaRepository.findAllTodos(id);
-		for(Ocorrencia c: ocorrencias) {
-			c.setImage(encoder(c.getPathFoto()));
-		}
-		return ocorrencias;
+//		List<Ocorrencia> ocorrencias = ocorrenciaRepository.findAllTodos(id);
+//		for(Ocorrencia c: ocorrencias) {
+//			c.setImage(encoder(c.getPathFoto()));
+//		}
+//		return ocorrencias;
+		return null;
 	}
 	
 	public Ocorrencia findByTipo(String tipo, int id) {
-		Ocorrencia ocorrencia = ocorrenciaRepository.findByTipo(tipo, id);
-		ocorrencia.setImage(encoder(ocorrencia.getPathFoto()));
-		return ocorrencia;
+//		Ocorrencia ocorrencia = ocorrenciaRepository.findByTipo(tipo, id);
+//		ocorrencia.setImage(encoder(ocorrencia.getPathFoto()));
+//		return ocorrencia;
+		return null;
 	}
 	
 	@Override
 	public String decode(String base64Image) {
-		String path;
-		Path currentPath = Paths.get("");
-		Path absolutePath = currentPath.toAbsolutePath();
-		String pathBase = absolutePath + "/src/main/resources/static/fotos/Imagem";
- 		path = geraPath(pathBase);
- 		
-		path = path.replace("\\", "/");
-
-		try (FileOutputStream imageOutFile = new FileOutputStream(path)) {	
- 			byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
-		    imageOutFile.write(imageByteArray);
-		} 
-		catch (FileNotFoundException e) {
-			System.out.println("Image not found" + e);
-		} 
-		catch (IOException ioe) {
-			System.out.println("Exception while reading the Image " + ioe);
-		}
-		return path;
+//		String path;
+//		Path currentPath = Paths.get("");
+//		Path absolutePath = currentPath.toAbsolutePath();
+//		String pathBase = absolutePath + "/src/main/resources/static/fotos/Imagem";
+// 		path = geraPath(pathBase);
+// 		
+//		path = path.replace("\\", "/");
+//
+//		try (FileOutputStream imageOutFile = new FileOutputStream(path)) {	
+// 			byte[] imageByteArray = Base64.getDecoder().decode(base64Image);
+//		    imageOutFile.write(imageByteArray);
+//		} 
+//		catch (FileNotFoundException e) {
+//			System.out.println("Image not found" + e);
+//		} 
+//		catch (IOException ioe) {
+//			System.out.println("Exception while reading the Image " + ioe);
+//		}
+//		return path;
+		return null;
 	}
 
 	//Padrão 'Imagem-1.jpg'
 	@Override
 	public String geraPath(String path) {
-		String idString;
- 		Integer id = ocorrenciaRepository.findLast();
- 		if(id != null)
- 			idString = String.valueOf(id+1);
- 		else
- 			idString = String.valueOf(01);
- 		
-		path = path + "-" + idString + ".png";	
-		return path;
+//		String idString;
+// 		Integer id = ocorrenciaRepository.findLast();
+// 		if(id != null)
+// 			idString = String.valueOf(id+1);
+// 		else
+// 			idString = String.valueOf(01);
+// 		
+//		path = path + "-" + idString + ".png";	
+//		return path;
+		return null;
 	}
 
 	@Override
 	public String encoder(String imagePath) {
-	    String base64Image = "";
-	    File file = new File(imagePath);
-	    try (FileInputStream imageInFile = new FileInputStream(file)) {
- 	      byte imageData[] = new byte[(int) file.length()];
-	      imageInFile.read(imageData);
-	      base64Image = Base64.getEncoder().encodeToString(imageData);
-	    } catch (FileNotFoundException e) {
-	      System.out.println("Image not found" + e);
-	    } catch (IOException ioe) {
-	      System.out.println("Exception while reading the Image " + ioe);
-	    }
-	    return base64Image;
+//	    String base64Image = "";
+//	    File file = new File(imagePath);
+//	    try (FileInputStream imageInFile = new FileInputStream(file)) {
+// 	      byte imageData[] = new byte[(int) file.length()];
+//	      imageInFile.read(imageData);
+//	      base64Image = Base64.getEncoder().encodeToString(imageData);
+//	    } catch (FileNotFoundException e) {
+//	      System.out.println("Image not found" + e);
+//	    } catch (IOException ioe) {
+//	      System.out.println("Exception while reading the Image " + ioe);
+//	    }
+//	    return base64Image;
+		return null;
 	  }
 }
